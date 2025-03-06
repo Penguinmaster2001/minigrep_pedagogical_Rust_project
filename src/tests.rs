59254c51a42cf0a153c286b7e81af089a141128b
Anthony Cieri <penguinmillion@gmail.com>
2025-03-05 19:32:05 -0800

    test: cast sensitivity tests

--- src/tests.rs
+++ src/tests.rs
@@ -37,4 +37,29 @@ Pick three.";
 
         assert_eq!(Vec::<&str>::new(), search(query, contents));
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "p";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "p";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive.", "Pick three."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
